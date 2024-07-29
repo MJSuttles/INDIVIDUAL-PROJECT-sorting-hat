57c167276e9c186bcfb0c1233ece4f1ba8ed70ed
Brian Suttles <rolltiderolldad@gmail.com>
2024-07-29 14:25:18 -0500

    Fixed issues creating cards from displaying on DOM. New cards can also be added andeleted.

--- main.js
+++ main.js
@@ -74,12 +74,19 @@ const form = document.querySelector("form");  // Targeting form element in HTML 
   
 const createStudent = (e) => {
   e.preventDefault(); // Prevents page from reloading prior to changes being made
+  const selectedRadioButton = document.querySelector('input[name="flexRadioDefault"]:checked');
+  console.log(selectedRadioButton);
+  const preferToGo = document.querySelector(`label[for="${selectedRadioButton.id}"]`).innerText;
+  console.log(preferToGo);
   const newStudentObj = {  // Creating variable to use to create new pet card object
     id: students.length +1,
     firstName: document.querySelector("#exampleFirstName").value,
     lastName: document.querySelector("#exampleLastName").value,
-    preferToDo: document.querySelector("#prefer").value
+    // preferToDo: document.querySelector('input[name="flexRadioDefault"]:checked').value
+    preferToDo: preferToGo
+  
   };
+  console.log(newStudentObj);
   students.push(newStudentObj);  // Pushes new student card object to end of existing petCards array
   cardsOnDom(students);  // Repaints DOM with new array
   form.reset();  // Resets form
@@ -94,7 +101,7 @@ houseCards.addEventListener("click", (e) => {  // adds Event Listener to listen 
     const [, id] = e.target.id.split("--");  // Destructures to split "ID" and card position, remove wording, and keep position number only
     const index = students.findIndex((e) => e.id === Number(id));  // Adds logic to remove from array via findIndex method - also makes sure that card being deleted matches the card's ID number
     students.splice(index, 1);  // modifies original array by removing index (the one deleted card) only
-
+    cardsOnDom(students);
   }
 });
 
@@ -128,14 +135,14 @@ showHufflepuffButton.addEventListener("click", () => {
 
 // Creating Listener Event to listen for when "Ravenclaw" button is clicked to show Ravenclaw students only
 
-showGryffindorButton.addEventListener("click", () => {
+showRavenclawButton.addEventListener("click", () => {
   const raven = filter(students, "What is wise");
   cardsOnDom(raven);
 });
 
 // Creating Listener Event to listen for when "Slytherin" button is clicked to show Slytherin students only
 
-showGryffindorButton.addEventListener("click", () => {
+showSlytherinButton.addEventListener("click", () => {
   const slyth = filter(students, "What is necessary");
   cardsOnDom(slyth);
 });
