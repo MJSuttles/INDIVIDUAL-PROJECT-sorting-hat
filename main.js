46eeff720b8ffefd7418eb0d0d4fe8f346d4121d
Brian Suttles <rolltiderolldad@gmail.com>
2024-08-01 19:20:24 -0500

    redesigned site based on description in README.md file.  Oops.

--- main.js
+++ main.js
@@ -4,58 +4,67 @@ const students = [
     image: "https://i.natgeofe.com/k/015ed957-e87b-403b-92a5-75372e8a28c3/a-8-harry-potter-harry_3x4.jpg",
     firstName: "Harry",
     lastName: "Potter",
-    preferToDo: "What is right"
+    house: "Gryffindor"
   },
   {
     id: 2,
     image: "https://media.harrypotterfanzone.com/hermione-granger-chamber-of-secrets-portrait-3.jpg",
     firstName: "Hermione",
     lastName: "Granger",
-    preferToDo: "What is right"
+    house: "Gryffindor"
   },
   {
     id: 3,
     image: "https://images.squarespace-cdn.com/content/v1/5a5d268364b05f7a0f038afe/1532024159009-FMF41SUE0A4Q8BEOU021/Voldemort.jpg",
     firstName: "Lord",
     lastName: "Voldemort",
-    preferToDo: "What is necessary"
+    house: "Slytherin"
   },
   {
     id: 4,
     image: "https://media.harrypotterfanzone.com/cedric-diggory-goblet-of-fire-portrait.jpg",
     firstName: "Cedric",
     lastName: "Diggory",
-    preferToDo: "What is kind"
+    house: "Hufflepuff"
   },
   {
     id: 5,
     image: "https://images.ctfassets.net/usf1vwtuqyxm/t6GVMDanqSKGOKaCWi8oi/74b6816d9f913623419b98048ec87d25/LunaLovegood_WB_F5_LunaLovegoodPromoCloseUp_Promo_080615_Port.jpg",
     firstName: "Luna",
     lastName: "Lovegood",
-    preferToDo: "What is wise"
+    house: "Ravenclaw"
   },
   {
     id: 6,
     image: "https://images.ctfassets.net/usf1vwtuqyxm/6o3lBfUzFoke5XPwn4vtdA/b3c3cbd509cb485515f616be8025d23f/FOUNDATIONAL-DISCOVER-FEATURE-newt-scamander-different-kind-of-hero-004PHOTOBU18181_PHUP_FB2.jpg",
     firstName: "Newt",
     lastName: "Scammander",
-    preferToDo: "What is kind"
+    house: "Hufflepuff"
   },
   {
     id: 7,
     image: "https://i.pinimg.com/474x/22/06/b9/2206b98fdd008df7f0b669cbb7711e0d.jpg",
     firstName: "Myrtle",
     lastName: "Warren",
-    preferToDo: "What is wise"
+    house: "Ravenclaw"
   },
   {
     id: 8,
     image: "https://upload.wikimedia.org/wikipedia/en/1/16/Draco_Mal.JPG",
     firstName: "Draco",
     lastName: "Malfoy",
-    preferToDo: "What is necessary"
+    house: "Slytherin"
   }
 ]
+
+const expelledStudents = [];
+
+const houseArray = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+
+const getRandomHouse = () => {
+  index = Math.floor(Math.random() * 3)
+  return houseArray[index];
+};
 
 // Utility (reusable) function to determine what element to target (divId) to use to render the House Cards to the DOM
 
@@ -66,30 +75,40 @@ const renderToDom = (divId, htmlToRender) => {
 
 // Creating array to render House cards on DOM via domString variable and then calling on utility function above to apply pet cards to the DOM via .innerHTML
 
-const cardsOnDom = (array) => {
+const studentsOnDom = (array) => {
   let domString = "";
   for (const student of array) {
     domString += `<div class="card" style="width: 18rem;">
     <h2 class="first-name">${student.firstName}</h2>
     <h2 class="last-name">${student.lastName}</h2>
-    <div class="card-body">
-      <image src=${student.image} class="card-img-top" alt=${student.name}></image>
-    </div>
-    <h2 class="house-student house-student-footer--${student.preferToDo}">${student.preferToDo}</h2>
     <button class="btn btn-danger" id="delete--${student.id}">Delete</button>
-    <button type="button" class="btn btn-dark">Expel!!</button>
+    <button type="button" class="btn btn-dark" id="expel--${student.id}">Expel!!</button>
   </div>`;
   }
   
   renderToDom("#houseCards", domString);
 };
 
+const expelledStudentsOnDom = (array) => {
+  let domString = "";
+  for (const student of array) {
+    domString += `<div class="card" style="width: 18rem;">
+    <h2 class="first-name">${student.firstName}</h2>
+    <h2 class="last-name">${student.lastName}</h2>
+    <button class="btn btn-danger" id="delete--${student.id}">Delete</button>
+    <button type="button" class="btn btn-dark" id="expel--${student.id}">Expel!!</button>
+  </div>`;
+  }
+  
+  renderToDom("#expelCards", domString);
+};
+
 // Creating filter function to filter results of array based on "preferToD" results via preferTo variable into available houses (Gryffindor, Hufflepuff, Ravenclaw,or Slytherin) and then returning the array
 
-const filter = (array, preference) => {
+const filter = (array, house) => {
   const preferArray = [];
   for (member of array) {
-    if (member.preferToDo === preference) {
+    if (member.house === house) {
       preferArray.push(member);
     }
   }
@@ -102,16 +121,15 @@ const form = document.querySelector("form");  // Targeting form element in HTML 
   
 const createStudent = (e) => {
   e.preventDefault(); // Prevents page from reloading prior to changes being made
-  const selectedRadioButton = document.querySelector('input[name="flexRadioDefault"]:checked');  // Checking for selected radio button choice
-  const newPreferToDo = document.querySelector(`label[for="${selectedRadioButton.id}"]`).innerText;  // Grabbing the text from the radio button to display on card
   const newStudentObj = {  // Creating variable to use to create new pet card object
     id: students.length +1,
+    // image: document.querySelector("#exampleFormControlFile1").value,
     firstName: document.querySelector("#exampleFirstName").value,
     lastName: document.querySelector("#exampleLastName").value,
-    preferToDo: newPreferToDo  // Reassigns preferToDo function to new card result based on answer to radio button question
+    house: getRandomHouse()
   };
   students.push(newStudentObj);  // Pushes new student card object to end of existing petCards array
-  cardsOnDom(students);  // Repaints DOM with new array
+  studentsOnDom(students);  // Repaints DOM with new array
   form.reset();  // Resets form
 }
 form.addEventListener("submit", createStudent);  // Creates listener event to apply new pet object entry when submit button is clicked
@@ -124,9 +142,22 @@ houseCards.addEventListener("click", (e) => {  // adds Event Listener to listen 
     const [, id] = e.target.id.split("--");  // Destructures to split "ID" and card position, remove wording, and keep position number only
     const index = students.findIndex((e) => e.id === Number(id));  // Adds logic to remove from array via findIndex method - also makes sure that card being deleted matches the card's ID number
     students.splice(index, 1);  // Modifies original array by removing index (the one deleted card) only
-    cardsOnDom(students);
+    studentsOnDom(students);
   }
 });
+
+houseCards.addEventListener("click", (e) => {
+  if (e.target.id.includes("expel")) {
+    const [, id] = e.target.id.split("--");
+    const index = students.findIndex((e) => e.id === Number(id));
+    const expelledStudent = students[index];
+    students.splice(index, 1);
+    expelledStudents.push(expelledStudent);
+    studentsOnDom(students);
+    expelledStudentsOnDom(expelledStudents);
+  }
+})
+
 
 // Creating Listener Event to listen for when "Show All Pets" button is clicked to show all students
 
@@ -139,39 +170,39 @@ const showSlytherinButton = document.querySelector("#showSlytherin");
 // Creating Listener Event to listen for when "Show All Pets" button is clicked to show all pets
 
 showAllButton.addEventListener("click", () => {
-  cardsOnDom(students);
+  studentsOnDom(students);
 });
 
 // Creating Listener Event to listen for when "Gryffindor" button is clicked to show Gryffindor students only
 
 showGryffindorButton.addEventListener("click", () => {
-  const gryff = filter(students, "What is right");
-  cardsOnDom(gryff);
+  const gryff = filter(students, "Gryffindor");
+  studentsOnDom(gryff);
 });
 
 // Creating Listener Event to listen for when "Hufflepuff" button is clicked to show Hufflepuff students only
 
 showHufflepuffButton.addEventListener("click", () => {
-  const huff = filter(students, "What is kind");
-  cardsOnDom(huff);
+  const huff = filter(students, "Hufflepuff");
+  studentsOnDom(huff);
 });
 
 // Creating Listener Event to listen for when "Ravenclaw" button is clicked to show Ravenclaw students only
 
 showRavenclawButton.addEventListener("click", () => {
-  const raven = filter(students, "What is wise");
-  cardsOnDom(raven);
+  const raven = filter(students, "Ravenclaw");
+  studentsOnDom(raven);
 });
 
 // Creating Listener Event to listen for when "Slytherin" button is clicked to show Slytherin students only
 
 showSlytherinButton.addEventListener("click", () => {
-  const slyth = filter(students, "What is necessary");
-  cardsOnDom(slyth);
+  const slyth = filter(students, "Slytherin");
+  studentsOnDom(slyth);
 });
 
 const startApp = () => {
-  cardsOnDom(students);
+  studentsOnDom(students);
 }
 
 startApp();
